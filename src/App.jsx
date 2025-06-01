df713a5f2b8e3fe4f25d5298fea9868fa0f1eff3
Yashpal568 <yash38687@gmail.com>
2025-06-01 13:59:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-600 text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
